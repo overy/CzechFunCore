2353aabc3dea495a8b16e069c44ef59251e4cd9a
locknes <locknes91@windowslive.com>
2011-05-24 21:04:02 +0200

    fix typo in TOC5 sql
    Signed-off-by: locknes <locknes91@windowslive.com>

--- sql/ChaosCore/world_TOC5.sql
+++ sql/ChaosCore/world_TOC5.sql
@@ -100,7 +100,7 @@ UPDATE `creature_template` SET `equipment_id`=235 WHERE `entry` in (34928,35517)
 UPDATE `creature_template` SET `equipment_id`=0 WHERE `entry` in (35451,35490);
 
 -- Scriptname, hp, damage, level
-UPDATE `creature_template` SET `ScriptName`='generic_vehicleAI_toc5', `minlevel`='80', `maxlevel`='80', `rank`='1', `mindmg`='420', `maxdmg`='630', `attackpower`='157', `Health_mod`='5,95238' WHERE `entry` IN (33324, 33321, 33322, 33320, 33323, 33319, 33318, 33317, 33316, 33324);
+UPDATE `creature_template` SET `ScriptName`='generic_vehicleAI_toc5', `minlevel`='80', `maxlevel`='80', `rank`='1', `mindmg`='420', `maxdmg`='630', `attackpower`='157', `Health_mod`='5.95238' WHERE `entry` IN (33324, 33321, 33322, 33320, 33323, 33319, 33318, 33317, 33316, 33324);
 
 -- Hp, damage and level.. 
 UPDATE `creature_template` SET `unit_flags`=0 , `minlevel`='80', `maxlevel`='80', `mindmg`='420', `rank`='1', `maxdmg`='630', `attackpower`='157', `Health_mod`='15' WHERE `entry` IN (35635, 35640, 35634, 35638, 35641, 34658, 35633, 35768, 35636, 35637);
