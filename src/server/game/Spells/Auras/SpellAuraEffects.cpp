3bf65b6a174c8f755e678694d71f446330f8c5fd
ZechY <ondra.davidek@seznam.cz>
2011-06-29 00:57:19 +0800

    Fix Tricks of the Trade

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6090,6 +6090,20 @@ void AuraEffect::HandleAuraDummy(AuraApplication const * aurApp, uint8 mode, boo
                     if (GetId() == 61777)
                         target->CastSpell(target, GetAmount(), true);
                     break;
+
+                 case SPELLFAMILY_ROGUE:
+               {
+                    switch(GetId())
+                    {
+                        case 59628: // Tricks of the Trade
+                            caster->SetReducedThreatPercent(0, 0);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                }
+
                 default:
                     break;
             }
