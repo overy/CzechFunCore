9ffcdb88e02db7fb7d1e973a37997c96943dbf11
overy.github.com <overy123@gmail.com>
2011-06-29 00:23:49 +0200

    Cleanup

--- sql/ChaosCore/world_AzjolNerub.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `creature_template` SET `ScriptName`='npc_krikthir_watcher' WHERE `entry`=28729;
-UPDATE `creature_template` SET `ScriptName`='npc_krikthir_watcher' WHERE `entry`=28730;
-UPDATE `creature_template` SET `ScriptName`='npc_krikthir_watcher' WHERE `entry`=28731;
-UPDATE `creature_template` SET `ScriptName`='npc_web_wrap' WHERE `entry`=28619;
-UPDATE `creature_template` SET `ScriptName`='npc_anubar_crusher' WHERE `entry`=28922;
